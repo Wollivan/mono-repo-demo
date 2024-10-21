30f074c9ab750fd053ff9d71403fc8673a560872
Tim <tim.smith2814@gmail.com>
2024-10-21 14:51:26 +0100

    Update API url

--- client/app.js
+++ client/app.js
@@ -1,7 +1,9 @@
 const h1 = document.querySelector("h1");
 
 async function updateh1() {
-  const response = await fetch("http://localhost:8080/message"); // making a GET request
+  const response = await fetch(
+    "https://mono-repo-demo-server.onrender.com/message"
+  ); // making a GET request
   const data = await response.json(); // get the data out of the promise
   h1.textContent = data;
 }
